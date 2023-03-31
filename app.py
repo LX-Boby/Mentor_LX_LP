2aab94afaad5eeb2891fbd3f5eb7b2a05a6db1a3
LearningX1D <lx.bobyhartanto@gmail.com>
2023-03-31 14:55:38 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient(
+    'mongodb+srv://nesa:nesasaputri@cluster0.7le98k4.mongodb.net/?retryWrites=true&w=majority')
+db = client.db_lx_pkl1
+
+
+@app.route('/')
+def main():
+    return render_template('index.html', title="Home")
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")
+
+
+@app.route('/mentors')
+def mentors():
+    return render_template('mentors.html', title="Mentors")
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title="Contact")
+
+
+@app.route('/auth')
+def auth():
+    return render_template('auth.html', title="User")
+
+
+@app.route('/detail/<keyword>')
+def detail(keyword):
+    print(keyword)
+    return render_template('detail.html', word=keyword)
+
+
+@app.route('/api/save_word', methods=['POST'])
+def save_word():
+    return jsonify({
+        'result': 'success',
+        'msg': 'the word was saved',
+    })
+
+
+@app.route('/api/delete_word', methods=['POST'])
+def delete_word():
+    return jsonify({
+        'result': 'success',
+        'msg': 'the word was deleted',
+    })
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
